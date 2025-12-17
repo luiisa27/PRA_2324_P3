19c1cf44428df69030d7ba50d7e088d5bc54aa80
Luisa Arango <lfarator@etsinf.upv.es>
2025-12-17 21:18:56 +0100

    añadida implementación d ela clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -3,55 +3,57 @@
 
 #include <string>
 #include <ostream>
-
+using namespace std;
+//TableEntry.h contendrá tanto la definición como la implementación de la clase TableEntry<V>
 template <typename V>
 class TableEntry {
-public:
-    std::string key;
-    V value;
-    bool hasValue;
+    public:
+        // atributos
+        std::string key; // clave del par. 
+        V value; // valor del par.
 
-    TableEntry(std::string key, V value)
-        : key(key), value(value), hasValue(true) {}
+        // métodos
+        //#1 Método constructor con el par clave->valor.
+        TableEntry(std::string key, V value){
+            this->key=key;
+            this->value=value;
+        }
 
-    TableEntry(std::string key)
-        : key(key), hasValue(false) {}
+        //#2 Crea una entrada solo con clave (sin valor). 
+        TableEntry(std::string key){
+            this->key=key;
+        }
 
-    TableEntry()
-        : key(""), hasValue(false) {}
+        //#3 Crea una entrada con la clave "" (cadena vacía) y sin valor.
+        TableEntry(){
+            this->key=" ";
+            this->value=V();
+        }
 
-    template <typename T>
-    friend bool operator==(const TableEntry<T> &, const TableEntry<T> &);
+        //#4 Sobrecarga global del operador == para determinar que dos instancias de TableEntry son iguales solo si comparten l		a misma clave (con independencia del valor).
+        friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+            return te1.key==te2.key;
+        }
 
-    template <typename T>
-    friend bool operator!=(const TableEntry<T> &, const TableEntry<T> &);
+        //#5 Sobrecarga global del operador != para determinar que dos instancias de TableEntry son diferentes solo si sus clav		es son distintas (con independencia del valor).
+        friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+            return te1.key!=te2.key;
 
-    template <typename T>
-    friend std::ostream& operator<<(std::ostream &, const TableEntry<T> &);
+        }
+
+        //#6 Sobrecarga global del operador << para imprimir el contenido de la entrada (par clave->valor) por pantalla. Recuer		da incluir la cabecera <ostream> en el .h.
+        friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
+            out << "clave: "<< te.key << "-> valor: " << te.value << endl;
+            return out;
+        }
+        // modificacion para comparación por clave
+        friend bool operator<(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return te1.key < te2.key;
+        }
+
+        friend bool operator>(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return te1.key > te2.key;
+        }
 };
 
-// operador ==
-template <typename V>
-bool operator==(const TableEntry<V> &a, const TableEntry<V> &b) {
-    return a.key == b.key;
-}
-
-// operador !=
-template <typename V>
-bool operator!=(const TableEntry<V> &a, const TableEntry<V> &b) {
-    return a.key != b.key;
-}
-
-// operador <<
-template <typename V>
-std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
-    if (te.hasValue)
-        out << te.key << " -> " << te.value;
-    else
-        out << te.key << " -> [NO VALUE]";
-    return out;
-}
-
 #endif
-
-
